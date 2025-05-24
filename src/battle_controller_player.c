30ebc6f7a1950e0e113d1ba6208e29ba050a553b
agsmgmaster64 <67435611+agsmgmaster64@users.noreply.github.com>
2025-05-24 08:42:26 -0400

    actually somewhat functions now

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -2340,6 +2340,7 @@ static void Controller_WaitForDebug(u32 battler)
 static void PlayerHandleBattleDebug(u32 battler)
 {
     BeginNormalPaletteFade(-1, 0, 0, 0x10, 0);
+    FreeAllWindowBuffers();
     UI_Battle_Menu_Init(ReshowBattleScreenAfterMenu); //battlemenu changes
     gBattlerControllerFuncs[battler] = Controller_WaitForDebug;
 }
