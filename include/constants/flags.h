6b483d5021d1647502bdd87a43b2bd9410a371c3
22ay <flyguyisaiaho@gmail.com>
2026-01-21 20:50:36 -0800

    fixed flags

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -47,8 +47,8 @@
 #define FLAG_DEXNAV_START_MENU    0x21 // Unused Flag
 #define FLAG_DEXNAV_DETECTOR    0x22 // Unused Flag
 #define FLAG_EXP_SHARE    0x23 // Unused Flag
-#define FLAG_UNUSED_0x024    0x24 // Unused Flag
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
+#define FLAG_DERBY_NICKNAME    0x24 // Unused Flag
+#define FLAG_DERBY_RESET    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
